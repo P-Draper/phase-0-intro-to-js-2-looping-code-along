72ee3431037966c8dac5b83a4db308b26bcb6d45
P-Draper <draperpa@gmail.com>
2023-05-15 13:22:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+  const messageCards = [];
+  for (let i = 0; i < cards.length; i++) {
+    messageCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  }
+
+    return messageCards;
+}
+
+writeCards(cards);
+
+function countDown() {
+  for(let i=10; i >= 0; i--){
+    console.log(i);
+  }
+}
+countDown();
